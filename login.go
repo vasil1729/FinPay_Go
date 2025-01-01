bdc545ee38939420a614bace6b0d6fc3ec0a75cc
Vasil <bumblebee47293@gmail.com>
2025-01-02 03:02:32 +0530

    feat: add a basic hello world server setup

---
+++ login.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Farriba!"))
+	})
+
+	fmt.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
